cb59e713b31f358059c297f293007b421bbc4c50
gq <guanqinghz@163.com>
2020-03-15 16:05:10 +0800

    初始化 actuator 项目

---
+++ sb-demo-03-actuator/src/main/java/com/guanqing/actuator/SbDemo03ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.guanqing.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDemo03ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDemo03ActuatorApplication.class, args);
+	}
+
+}
