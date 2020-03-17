fce2e1ed7bc705759acb509f176572ac6ef6d512
gq <guanqinghz@163.com>
2020-03-17 14:36:42 +0800

    修改.gitignore文件

---
+++ sb-demo-06-log-aop/src/main/java/com/guanqing/log/aop/SbDemo06LogAopApplication.java
@@ -0,0 +1,13 @@
+package com.guanqing.log.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDemo06LogAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDemo06LogAopApplication.class, args);
+	}
+
+}
