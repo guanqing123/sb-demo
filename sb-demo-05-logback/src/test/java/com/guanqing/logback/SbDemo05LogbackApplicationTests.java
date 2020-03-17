5a11c099488b72bed9f52e3550c26a26d86ca518
gq <guanqinghz@163.com>
2020-03-17 08:43:03 +0800

    初始化 logback

---
+++ sb-demo-05-logback/src/test/java/com/guanqing/logback/SbDemo05LogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guanqing.logback;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDemo05LogbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
